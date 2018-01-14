0bf9d7325514b47f3dd9103c84867f2f576a6909
alexponce <alex@elaniin.com>
2018-01-14 12:51:57 -0600

    add relation with courses  in serializer class

--- app/serializers/cms/groups/group_serializer.rb
+++ app/serializers/cms/groups/group_serializer.rb
@@ -3,6 +3,7 @@ module CMS
     class GroupSerializer < ActiveModel::Serializer
       attributes :id, :name, :description, :created_at
       has_many :courses
+    
       def created_at
         object.created_at.iso8601
       end
