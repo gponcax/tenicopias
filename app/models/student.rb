18693c41ad7a75198c7277bde584c4316ba6b565
alexponce <alex@elaniin.com>
2018-01-15 02:24:11 -0600

    fix it config paperclip in model student and add cnahes in serialier doc

--- app/models/student.rb
+++ app/models/student.rb
@@ -3,9 +3,9 @@
          :recoverable, :rememberable, :trackable, :validatable
    has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
 
-   validates_attachment_content_type :avatar, { content_type: ['image/jpeg', 'image/png'] },
-                                     size: { in: 0..3.megabytes },
-                                     presence: true
+   validates_attachment :avatar,
+                        content_type: {content_type: ['image/jpeg', 'image/png'] },
+                        size: { in: 0..3.megabytes }, presence: true
 
     has_many :groups
     has_many :docs, through: :claims
