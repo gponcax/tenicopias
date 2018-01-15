ff30dd042f0e9e292288e3a27b0da46148cac8d1
alexponce <alex@elaniin.com>
2018-01-14 23:39:21 -0600

    add relationships teacher with courses

--- app/serializers/cms/teachers/teacher_serializer.rb
+++ app/serializers/cms/teachers/teacher_serializer.rb
@@ -3,6 +3,7 @@ module CMS
     class TeacherSerializer < ActiveModel::Serializer
       attributes :id, :name, :email, :phone, :created_at
 
+      has_many :courses
       def created_at
         object.created_at.iso8601
       end
