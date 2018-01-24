e548e4d1a7d960beaab0c1f8047a4e296eb0fee3
alexponce <alex@elaniin.com>
2018-01-24 05:09:17 -0600

    add teaacher to couses

--- app/serializers/cms/courses/course_serializer.rb
+++ app/serializers/cms/courses/course_serializer.rb
@@ -2,7 +2,7 @@ module CMS
   module Courses
     class CourseSerializer < ActiveModel::Serializer
       attributes :id, :name
-  
+      belongs_to :teacher
       def created_at
         object.created_at.iso8601
       end
