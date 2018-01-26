8a7a2da03f0e169d1fed80b068098d0600e7f9d7
alexponce <alex@elaniin.com>
2018-01-25 23:14:44 -0600

    Add filename to docs json

--- app/serializers/cms/docs/doc_serializer.rb
+++ app/serializers/cms/docs/doc_serializer.rb
@@ -2,7 +2,7 @@ module CMS
   module Docs
     class DocSerializer < ActiveModel::Serializer
       attributes  :id, :name, :description, :document, :mime_type, :file_size,
-                  :price, :page_numbers
+                  :price, :page_numbers, :document_file_name
        belongs_to :course
       def created_at
         object.created_at.iso8601
