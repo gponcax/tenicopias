9184d7187a20be4badec902dbf51e9345297b3fa
alexponce <alex@elaniin.com>
2018-01-28 00:03:25 -0600

    add star and end time to courses for schedule courses

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180128041445) do
+ActiveRecord::Schema.define(version: 20180128060223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,8 @@ ActiveRecord::Schema.define(version: 20180128041445) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "teacher_id"
+    t.datetime "start_time"
+    t.datetime "end_time"
     t.index ["teacher_id"], name: "index_courses_on_teacher_id"
   end
 
